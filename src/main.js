6e52fb8448ae31bee624b4899d8a4c447f4f43f0
Adem Furkan Özcan <ozcanfurkan@gmail.com>
2021-02-23 00:38:36 +0300

    pwa init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import vuetify from '@/plugins/vuetify' // path to vuetify export
 import Player from './Player.vue'
 import VueMeta from 'vue-meta';
+import './registerServiceWorker'
 
 Vue.use(VueMeta)
 
